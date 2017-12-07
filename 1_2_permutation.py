0da6897798f52a232a2ef4b738bbb8e1e3c8aaff
Yuriy Bash <yuriybash@gmail.com>
2017-12-07 11:30:11 -0400

    adds 1_2_permutation

---
+++ 1_2_permutation.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def is_permutation(s1, s2):
+	
+	s1_store = defaultdict(lambda: 0)
+	for c in s1:
+		s1_store[c] += 1
+	for c in s2:
+		s1_store[c] -= 1
+		if s1_store[c] < 0:
+			return False
+	return True
