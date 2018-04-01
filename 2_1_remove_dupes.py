f556d323ee0f082c536506fbabf521bb3da7c18c
Yuriy Bash <yuriybash@gmail.com>
2018-04-01 14:33:02 -0400

    adds 2_1 with no addtl memory

--- 2_1_remove_dupes.py
+++ 2_1_remove_dupes.py
@@ -25,3 +25,19 @@ def remove_dupes(node):
             count[curr_node.next.val] -= 1
             curr_node.next = curr_node.next.next
         curr_node = curr_node.next
+
+
+def remove_dupes_no_addtl_mem(node):
+
+    first_ptr = node
+    while first_ptr:
+
+        # curr_val = first_ptr.val
+
+        second_ptr = first_ptr
+        while second_ptr:
+            if second_ptr.next and second_ptr.next.val == first_ptr.val:
+                second_ptr.next = second_ptr.next.next
+            second_ptr = second_ptr.next
+
+        first_ptr = first_ptr.next
