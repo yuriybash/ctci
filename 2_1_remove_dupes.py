9ce8f2fcd5717b83385fd4808a03316e84d69421
Yuriy Bash <yuriybash@gmail.com>
2018-04-02 09:30:22 -0400

    minor cleanup on 2_1

--- 2_1_remove_dupes.py
+++ 2_1_remove_dupes.py
@@ -31,13 +31,9 @@ def remove_dupes_no_addtl_mem(node):
 
     first_ptr = node
     while first_ptr:
-
-        # curr_val = first_ptr.val
-
         second_ptr = first_ptr
         while second_ptr:
             if second_ptr.next and second_ptr.next.val == first_ptr.val:
                 second_ptr.next = second_ptr.next.next
             second_ptr = second_ptr.next
-
         first_ptr = first_ptr.next
