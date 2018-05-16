7d1becbef6e2a77eb3aa265db779dce34c01a825
Yuriy Bash <yuriybash@gmail.com>
2018-05-16 03:33:12 -0400

    renames 2_3 var to avoid shadowing builtin

--- 2_3_delete_middle_node.py
+++ 2_3_delete_middle_node.py
@@ -1,8 +1,8 @@
 class Node(object):
 
-    def __init__(self, val, next=None):
+    def __init__(self, val, next_=None):
         self.val = val
-        self.next = next
+        self.next_ = next_
 
     def __repr__(self):
         return 'Node(x=%s)' % self.val
@@ -10,10 +10,10 @@ class Node(object):
 
 def delete_middle_node(node):
 
-    temp = node.next.val
-    while node.next:
+    temp = node.next_.val
+    while node.next_:
         node.val = temp
-        if not node.next.next:
+        if not node.next_.next_:
             break
-        temp = node.next.next.val
-        node = node.next
+        temp = node.next_.next_.val
+        node = node.next_
